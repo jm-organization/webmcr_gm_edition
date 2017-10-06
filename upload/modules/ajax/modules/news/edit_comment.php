4c55776fc5b90d6d391d5ad26a94b270895d4f51
Magicfar4 <vany.58.98.2013@gmail.com>
2017-10-06 16:10:33 +0300

    Исправлен вывод новостей. Настроена обрезка новости до "краткого описания". Ибрана поддержка bb-кодов в новостях. Исправлен вывод, добавление, редактирование и удаление коментариев.

--- upload/modules/ajax/modules/news/edit_comment.php
+++ upload/modules/ajax/modules/news/edit_comment.php
@@ -27,46 +27,55 @@ class submodule{
 
 	public function content(){
 
-		if($_SERVER['REQUEST_METHOD']!='POST'){ $this->core->js_notify($this->core->lng['e_hack']); }
-		
-		if(!$this->core->is_access('sys_comment_edt') && !$this->core->is_access('sys_comment_edt_all')){ $this->core->js_notify($this->lng['com_perm_edit']); }
-
-		$id = intval(@$_POST['id']);
-
-		$nid = intval(@$_POST['nid']);
-
-		if(!$this->is_discus($nid)){ $this->core->js_notify($this->lng['com_disabled']); }
-
-		$sql_query = "SELECT `data` FROM `mcr_comments` WHERE uid='{$this->user->id}' AND id='$id' AND nid='$nid'";
-
-		if($this->core->is_access('sys_comment_edt_all')){
-			$sql_query = "SELECT `data` FROM `mcr_comments` WHERE id='$id' AND nid='$nid'";
+		if ($_SERVER['REQUEST_METHOD']!='POST') {
+			$this->core->js_notify($this->core->lng['e_hack']);
+		}
+		if (!$this->core->is_access('sys_comment_edt')
+		    && !$this->core->is_access('sys_comment_edt_all')) {
+			$this->core->js_notify($this->lng['com_perm_edit']);
 		}
 
+		$id = intval(@$_POST['id']);
+		$nid = intval(@$_POST['nid']);
+
+		if (!$this->is_discus($nid)) {
+			$this->core->js_notify($this->lng['com_disabled']);
+		}
+
+		$sql_query = ($this->core->is_access('sys_comment_edt_all'))?"
+			SELECT 
+				`data` 
+			FROM `mcr_news_comments` 
+			WHERE uid='{$this->user->id}' AND id='$id' AND nid='$nid'
+			":"
+			SELECT 
+				`data` 
+			FROM `mcr_news_comments` 
+			WHERE id='$id' AND nid='$nid'
+		";
 		$query = $this->db->query($sql_query);
 
-		if(!$query || $this->db->num_rows($query)<=0){ $this->core->js_notify($this->core->lng['e_hack']); }
+		if (!$query || $this->db->num_rows($query) <= 0) {
+			$this->core->js_notify($this->core->lng['e_hack']);
+		}
 
 		$ar = $this->db->fetch_assoc($query);
-
 		$data = json_decode($ar['data']);
-
 		$message = @$_POST['message'];
+		$message = trim($message);
 
-		$message_trim = trim($message);
-
-		if(empty($message_trim)){ $this->core->js_notify($this->lng['com_msg_empty']); }
+		if (empty($message)) { $this->core->js_notify($this->lng['com_msg_empty']); }
 
 		$bb = $this->core->load_bb_class(); // Object
 
-		$text_html		= $bb->parse($message);
+		$text_html = $bb->parse($message);
 		$safe_text_html	= $this->db->safesql($text_html);
-
-		$text_bb		= $this->db->safesql($message);
-
+		$text_bb = $this->db->safesql($message);
 		$message_strip = trim(strip_tags($text_html, "<img><hr><iframe>"));
 
-		if(empty($message_strip)){ $this->core->js_notify($this->lng['com_msg_incorrect']); }
+		if (empty($message_strip)) {
+			$this->core->js_notify($this->lng['com_msg_incorrect']);
+		}
 
 		$newdata = array(
 			"time_create" => $data->time_create,
@@ -74,23 +83,19 @@ class submodule{
 		);
 
 		$safedata = $this->db->safesql(json_encode($newdata));
-
-		$sql_update = "UPDATE `mcr_comments`
-						SET text_html='$safe_text_html',
-							text_bb='$text_bb',
-							`data`='$safedata'
-						WHERE id='$id' AND nid='$nid' AND uid='{$this->user->id}'";
-
-		if($this->core->is_access('sys_comment_edt_all')){
-
-			$sql_update = "UPDATE `mcr_comments`
-							SET text_html='$safe_text_html',
-								text_bb='$text_bb',
-								`data`='$safedata'
-							WHERE id='$id' AND nid='$nid'";
-
-		}
-
+		$sql_update = ($this->core->is_access('sys_comment_edt_all'))?"
+			UPDATE `mcr_news_comments`
+			SET text_html='$safe_text_html',
+				text_bb='$text_bb',
+				`data`='$safedata'
+			WHERE id='$id' AND nid='$nid' AND uid='{$this->user->id}'
+		":"
+			UPDATE `mcr_news_comments`
+			SET text_html='$safe_text_html',
+				text_bb='$text_bb',
+				`data`='$safedata'
+			WHERE id='$id' AND nid='$nid'
+		";
 		$update = $this->db->query($sql_update);
 
 		if(!$update){ $this->core->js_notify($this->core->lng['e_sql_critical']); }
