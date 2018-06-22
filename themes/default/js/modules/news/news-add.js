52043a6f7234ece8c1af21fe4915ef92bf588db3
GhostlyR <rainbow.ghostly@yandex.ru>
2018-06-23 01:43:45 +0600

    Semantic-UI: News add form

--- themes/default/js/modules/news/news-add.js
+++ themes/default/js/modules/news/news-add.js
@@ -1,11 +1,3 @@
-function check_switch(checkbox, callback_true, callback_false) {
-	if ($(checkbox).is(':checked')) {
-		callback_true();
-	} else {
-		callback_false();
-	}
-}
-
 $(document).ready(function () {
 	mcr.init_database('#news_list', {
 		searching: true,
@@ -29,48 +21,22 @@ $(document).ready(function () {
 		pagebreak_split_block: true,
 		plugins: "code preview image imagetools link textcolor wordcount pagebreak",
 		menubar: "file edit insert view format table tools",
-		toolbar1: "bold italic strikethrough | formatselect fontsizeselect | backcolor forecolor",
-		toolbar2: "alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | link image | pagebreak | removeformat",
+		toolbar1: "bold italic strikethrough | formatselect fontsizeselect | backcolor forecolor | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | link image | pagebreak | removeformat",
 		pagebreak_separator: "{READMORE}"
 	});
 
-	// <<JS::SWITCH
-	$('.switch').on(
-		'click',
-		'[name="planed_publish"]'
-		+ ', [name="closed_comments"]',
-		function () {
-			switch (this.name) {
-				case 'planed_publish':
-					check_switch('[name="planed_publish"]', function () {
-						$('#date_publish').show();
-					}, function () {
-						$('#date_publish').hide();
-					});
-					break;
-				case 'closed_comments':
-					check_switch('[name="closed_comments"]', function () {
-						$('#date_cs').show();
-					}, function () {
-						$('#date_cs').hide();
-					});
-					break;
-				default:
-					break;
-			}
-		}
-	);
-	check_switch('[name="planed_publish"]', function () {
-		$('#date_publish').show();
-	}, function () {
-		$('#date_publish').hide();
-	});
-	check_switch('[name="closed_comments"]', function () {
-		$('#date_cs').show();
-	}, function () {
-		$('#date_cs').hide();
-	});
-	// >>
+	// Активируем триггер показа блока запланированной публикации
+	$('#planed_publish')
+		.checkbox()
+		.first().checkbox({
+		onChecked: function () {
+			$('#date_publish').show();
+		},
+		onUnchecked: function () {
+			$('#date_publish').hide();
+		},
+	})
+	;
 
 	// DataTime Picker
 	$('#input_publish_time, #input_date_cs').datetimepicker({
