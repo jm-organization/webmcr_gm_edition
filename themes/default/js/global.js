0d63865a9f4b5d0b11ba37d7cd18030300b971f4
GhostlyR <rainbow.ghostly@yandex.ru>
2018-06-23 09:45:58 +0600

    Add doT.js template engine

--- themes/default/js/global.js
+++ themes/default/js/global.js
@@ -403,6 +403,19 @@ var mcr = {
 	}
 };
 
+$.fn.tmpl = function(tmplId, data) {
+	var tmpl = doT.template($('#' + tmplId).text());
+	if (!$.isArray(data)) data = [data];
+
+	return this.each(function() {
+		var html = '';
+		for (var itemIdx = 0; itemIdx < data.length; itemIdx++) {
+			html += tmpl(data[itemIdx]);
+		}
+		$(this).html(html);
+	});
+};
+
 // Функции, вызываемые при загрузке
 $(function () {
 
