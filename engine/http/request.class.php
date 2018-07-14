59519d604e551ea8d836cc8cd4d01066be5a6acb
Magicfar4 <vany.58.98.2013@gmail.com>
2018-07-14 22:04:38 +0300

    Impruve request class. Add methods and fileds for quick use it

--- engine/http/request.class.php
+++ engine/http/request.class.php
@@ -21,18 +21,24 @@ class request
 	 */
 	private $attributes = [];
 
+	public static $host = '';
+
+	public static $path = '';
+
 	/*
 	 * Constructor for request
 	 */
-	/**
-	 * request constructor.
-	 *
-	 *
-	 *
-	 * @documentation:
-	 */
-	public function __construct() {
-		$this->attributes = array_merge($this->attributes, $_POST);
+	public function __construct()
+	{
+		$this->set_attributes();
+
+		self::set_host();
+		self::set_path();
+	}
+
+	private function set_attributes()
+	{
+		$this->attributes = array_merge($this->attributes, $_POST, $_GET);
 
 		// Очишаем пришедшие данные от пробелов в начале и конце.
 		$this->attributes = array_map(function($attr) {
@@ -44,15 +50,37 @@ class request
 		}
 	}
 
+	private static function set_host()
+	{
+		self::$host = $_SERVER['SERVER_NAME'];
+	}
+
+	private static function set_path()
+	{
+		$uri = self::uri();
+
+		if (false !== $pos = strpos($uri, '?')) {
+			$uri = substr($uri, 0, $pos);
+		}
+
+		$path = rawurldecode($uri);
+
+		self::$path = $path;
+	}
+
 	/**
 	 * Помещает данные в запрос.
 	 * Данные отдаёт роутер.
 	 *
 	 * @param array $attributes
+	 *
+	 * @return request
 	 */
 	public function put(array $attributes)
 	{
 		$this->attributes = array_merge($this->attributes, $attributes);
+
+		return $this;
 	}
 
 	/**
@@ -63,6 +91,35 @@ class request
 	public function all()
 	{
 		return $this->attributes;
+	}
+
+	/**
+	 * Возвращает данные, о методе запроса
+	 *
+	 * @return mixed
+	 */
+	public static function method()
+	{
+		return $_SERVER['REQUEST_METHOD'];
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function url()
+	{
+		$_base_url = router::base_url();
+		$_request_uri = $_SERVER['REQUEST_URI'];
+
+		return $_base_url . $_request_uri;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public static function uri()
+	{
+		return $_SERVER['REQUEST_URI'];
 	}
 
 	/**
@@ -93,34 +150,5 @@ class request
 		}
 
 		return null;
-	}
-
-	/**
-	 * Возвращает данные, о методе запроса
-	 *
-	 * @return mixed
-	 */
-	public static function method()
-	{
-		return $_SERVER['REQUEST_METHOD'];
-	}
-
-	/**
-	 * @return string
-	 */
-	public static function url()
-	{
-		$_base_url = router::base_url();
-		$_request_uri = $_SERVER['REQUEST_URI'];
-
-		return $_base_url . $_request_uri;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public static function uri()
-	{
-		return $_SERVER['REQUEST_URI'];
 	}
 }
