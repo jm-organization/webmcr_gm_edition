ebb7bffe4fc87becb1b545d26d83c6265be1419f
Magicfar4 <vany.58.98.2013@gmail.com>
2018-09-07 20:19:12 +0300

    Add Captcha driver

---
+++ src/mcr/captcha/drivers/captcha_recaptcha_driver.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright (c) 2018.
+ * MagicMCR является отдельным и независимым продуктом.
+ * Исходный код распространяется под лицензией GNU General Public License v3.0.
+ *
+ * MagicMCR не является копией оригинального движка WebMCR, а лишь его подверсией.
+ * Разработка MagicMCR производится исключительно в частных интересах. Разработчики, а также лица,
+ * участвующие в разработке и поддержке, не несут ответственности за проблемы, возникшие с движком.
+ */
+
+/**
+ * Created in JM Organization.
+ *
+ * @e-mail: admin@jm-org.net
+ * @Author: Magicmen
+ *
+ * @Date  : 26.08.2018
+ * @Time  : 13:36
+ */
+
+namespace mcr\captcha\drivers;
+
+
+class captcha_recaptcha_driver
+{
+}
