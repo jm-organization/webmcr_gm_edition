b448d113d7be980820919585ab686c0f53111965
Magicfar4 <vany.58.98.2013@gmail.com>
2018-09-09 18:02:02 +0300

    Add admin page of phrases list. Integrate Datatbles to application. Change styles

--- themes/default/assets/js/modules/l10n/phrases.js
+++ themes/default/assets/js/modules/l10n/phrases.js
@@ -1,24 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Copyright (c) 2018.
+ * MagicMCR является отдельным и независимым продуктом.
+ * Исходный код распространяется под лицензией GNU General Public License v3.0.
+ *
+ * MagicMCR не является копией оригинального движка WebMCR, а лишь его подверсией.
+ * Разработка MagicMCR производится исключительно в частных интересах. Разработчики, а также лица,
+ * участвующие в разработке и поддержке, не несут ответственности за проблемы, возникшие с движком.
  */
 
-/* global mcr */
+(function (windows, document, jquery)
+{
+    let app = windows.app;
 
-$(document).ready(function () {
-	mcr.init_database('#phrases', {
-		searching: true,
-		language: {
-			url: '/language/ru-RU/js/database.json'
-		}
-	});
+    let $table = $('table#phrases').magicTable({
+        lengthMenu: [[50, 75, 150, 250, -1], [50, 75, 150, 250, "Все"]],
+        pageLength: 50,
+        columns: [
+            { "orderable": true },
+            { "orderable": false }
+        ]
+    });
 
-	$('#to_language').on('change', '', function (event) {
-		var href = '/?mode=admin&do=l10n_phrases&language=' + event.target.value;
-
-		top.location.href = href;
-	});
-});
-
+    /*app.ajax({
+        url: ''
+    });*/
+}
+)(window, document, jQuery);
 
